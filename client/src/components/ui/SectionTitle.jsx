b3d1813860319446e8bdbd52f9d10db123b3ec9c
Otávio Vinícius Flauzino <159206555+TavinV@users.noreply.github.com>
2025-09-15 12:29:17 -0300

    Atualizar o SectionTitle.jsx

--- client/src/components/ui/SectionTitle.jsx
+++ client/src/components/ui/SectionTitle.jsx
@@ -1,14 +1,26 @@
-const SectionTitle = ({text}) => {
-    return (
-        <div className="flex flex-col justify-center text-center py-10">
-            <h1 className="font-bold text-5xl uppercase font-highlight">{text}</h1>
-            <div className="flex items-center w-full justify-center">
-                <hr className="w-[100px] border-1 border-primary" />
-                <img src="/star.svg" className="w-10 h-10" />
-                <hr className="w-[100px] border-1 border-primary" />
-            </div>
-        </div>
-    );
-}
+const SectionTitle = ({ text }) => {
+  return (
+    <div className="flex flex-col justify-center text-center py-10 px-4">
+      <h1
+        className="
+          font-bold 
+          uppercase 
+          font-highlight 
+          text-[clamp(1.5rem,5vw,3rem)] 
+          max-w-[250px] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] 
+          mx-auto
+        "
+      >
+        {text}
+      </h1>
 
-export default SectionTitle
+      <div className="flex items-center w-full justify-center mt-4">
+        <hr className="w-[100px] border-1 border-primary" />
+        <img src="/star.svg" className="w-10 h-10 mx-2" />
+        <hr className="w-[100px] border-1 border-primary" />
+      </div>
+    </div>
+  );
+};
+
+export default SectionTitle;
