896828bbb3d6de172e6eabaf23dadbd459f728be
Otávio Vinícius Flauzino <159206555+TavinV@users.noreply.github.com>
2025-09-15 11:08:57 -0300

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://dalia-semijoias-api.onrender.com", // o proxy no package.json já redireciona para http://localhost:3000
+    baseURL: "https://dalia-semijoias-api.onrender.com/api/v1", // o proxy no package.json já redireciona para http://localhost:3000
 });
 
 export default api;
