970ce5f397fb1a7095f9bde1b8374694595cf902
Otávio Vinícius Flauzino <159206555+TavinV@users.noreply.github.com>
2025-09-15 12:31:41 -0300

    Atualizar o SectionTitle.jsx

--- client/src/components/ui/SectionTitle.jsx
+++ client/src/components/ui/SectionTitle.jsx
@@ -8,13 +8,14 @@ const SectionTitle = ({ text }) => {
           font-highlight 
           text-[clamp(1.5rem,5vw,3rem)] 
           max-w-[250px] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] 
-          mx-auto
+          mx-auto 
+          mb-2
         "
       >
         {text}
       </h1>
 
-      <div className="flex items-center w-full justify-center mt-4">
+      <div className="flex items-center w-full justify-center">
         <hr className="w-[100px] border-1 border-primary" />
         <img src="/star.svg" className="w-10 h-10 mx-2" />
         <hr className="w-[100px] border-1 border-primary" />
@@ -24,3 +25,4 @@ const SectionTitle = ({ text }) => {
 };
 
 export default SectionTitle;
+
